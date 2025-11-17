e274afcb7ae00578508888a0e04263308090f951
vidc01 <victor.dct01@gmail.com>
2025-11-17 12:41:50 +0100

    last

--- 1.3/index.php
+++ 1.3/index.php
@@ -1,19 +1,17 @@
 <?php
 
-$n = 5;
 function doubleLoop($n) {
-
     for ($i = 1; $i <= $n; $i++) {
-
-        // Cette boucle va répéter le chiffre i exactement i fois
+        // Répéter le chiffre i exactement i fois
         for ($j = 1; $j <= $i; $j++) {
             echo $i;
         }
-
         // Retour à la ligne après chaque séquence
         echo "<br>";
     }
-
 }
 
-doubleLoop($n);
+// Appel de la fonction avec 5 par défaut si aucune valeur n'est envoyée via POST
+doubleLoop($_POST['n'] ?? 5);
+
+?>
