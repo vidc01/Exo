433c23211e24ca90c4fad1146ad09807644ec22c
vidc01 <victor.dct01@gmail.com>
2025-11-18 15:28:15 +0100

    ajout exo

--- 4.1/index.php
+++ 4.1/index.php
@@ -3,12 +3,10 @@ $eleves = [
     "Alice" => [15, 12, 14],
     "Bob" => [10, 14, 12],
     "Claire" => [18, 17, 19]
-];
- 
+]; 
 function calcule($tableau) {
     return array_sum($tableau) / count($tableau);
 }
- 
 foreach ($eleves as $nom => $notes) {
     $moyenne = calcule($notes);
     echo "Nom : $nom - Moyenne : $moyenne <br>";
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+function strcontain($string, $chaine){
+    $very = 0;
+    for($i=0;$i<=strlen($chaine);$i++){
+        if ($chaine[$i]==$string[$very]){
+            $very++;
+        }
+        if($very ==strlen($string) ){
+            return "Yes";
+        }
+    }
+    return "No";
+}
+ 
+echo strcontain("CEF", "ABCEFGHI")
+?>
